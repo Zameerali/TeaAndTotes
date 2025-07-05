3280d425125b3d3e70291f2be8b8763863628096
Zameerali <zameeralimustafa@gmail.com>
2025-07-06 02:24:29 +0500

    added dockerfile for fly.io

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ import imageRoutes from './routes/images.js';
 import { connectDB } from './config/db.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -26,7 +26,3 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/images', imageRoutes);
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
