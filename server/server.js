a95e4a013a1d32088df4f37479e47f0cb66d076d
Zameerali <zameeralimustafa@gmail.com>
2025-07-06 03:37:10 +0500

    changes

--- server/server.js
+++ server/server.js
@@ -9,11 +9,16 @@ import cartRoutes from './routes/cart.js';
 import imageRoutes from './routes/images.js';
 import { connectDB } from './config/db.js';
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 // Middleware
 app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'], // Update with Netlify URL
+  credentials: true,
+}));
 app.use(express.json());
 
 // Connect to MongoDB
