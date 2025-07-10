cc34922830b7436053d51b9a8d57836ef9134176
Zameerali <zameeralimustafa@gmail.com>
2025-07-10 13:27:14 -0700

    updated category

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -23,9 +23,8 @@ function Products() {
             className="border rounded px-4 py-2 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 shadow"
           >
             <option value="">All Categories</option>
-            <option value="Eco">Eco</option>
+            <option value="Eco">Customised</option>
             <option value="Canvas">Canvas</option>
-            <option value="Leather">Leather</option>
           </select>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
